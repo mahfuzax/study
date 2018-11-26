4c6ef2bb1c48497d7d30b6f7c2ef4e02ca6a2ebd
Mahfuzur Rahman <mahfuzur@augmedix.com>
2018-11-26 19:44:47 +0600

    Fixed issue with merge method

--- mitOcw/mergeSort.cpp
+++ mitOcw/mergeSort.cpp
@@ -1,22 +1,35 @@
-#include<iostream>
+#include <stdio.h>
+#define MAX 99999999
 
 using namespace std;
+
+void print(int* arr, int len) {
+    printf("============================\n");
+    for(int i = 0; i < len; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n============================\n");
+}
 
 void merge(int a[], int p, int q, int r){
 	int n1 = q - p + 1;
 	int n2 = r - q;
+
 	int *left = new int[n1 + 1];
 	int *right = new int [n2 + 1];
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; i <= n1; i++)
-		left[i] = a[p + i - 1];
-	for (j = 0; j <= n2; j++)
-		right[j] = a[q + j];
+	for (i = 0; i < n1; i++)
+		left[i] = a[p + i];
+	for (j = 0; j < n2; j++)
+		right[j] = a[q + 1 + j];
 
-	left[n1] = 99999;
-	right[n2] = 99999;
+	left[n1] = MAX;
+	right[n2] = MAX;
+
+	print(left, n1 + 1);
+	print(right, n2 + 1);
 
 	i = 0;
 	j = 0;
@@ -44,11 +57,9 @@ void mergeSort(int a[], int p, int r){
 }
 
 
-void main(){
+int main(){
 	int a[] = { 1, 2, 3, 6, 5, 4 };
 	mergeSort(a, 0, 5);
-	for (int i = 0; i < 6; i++){
-		printf("%d ", a[i]);
-	}
-
+	print(a, 6);
+    return 0;
 }
